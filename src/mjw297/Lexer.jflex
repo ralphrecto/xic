43099d055914990a93bb9f93ebc5d80f08cfd532
Michael Whittaker <mjw297@cornell.edu>
2016-02-07 14:51:46 +0000

    Added simple string test cases.
    I also made the lexer use the correct start row:col for strings.

--- src/mjw297/Lexer.jflex
+++ src/mjw297/Lexer.jflex
@@ -15,24 +15,32 @@ import java_cup.runtime.*;
 %state STRING CHARACTER
 
 %{
+    // strings are lexed into sb
     StringBuffer sb = new StringBuffer();
+    // If not -1, the column of the start of a char or string
+    int startColumn = -1;
+
+    private int row() {
+        return yyline + 1;
+    }
+
+    private int column() {
+        return yycolumn + 1;
+    }
 
     private Symbol symbol(int type) {
-        return new Symbol(type, yyline + 1, yycolumn + 1);
+        return new Symbol(type, row(), column());
     }
 
     private Symbol symbol(int type, Object value) {
-        return new Symbol(type, yyline + 1, yycolumn + 1, value);
+        return new Symbol(type, row(), column(), value);
     }
 %}
-
-
-
 
 %%
 
 <YYINITIAL> {
-	
+
 	/* Keywords */
 	"while"		{ return symbol(Sym.WHILE);  }
 	"if"		{ return symbol(Sym.IF);     }
@@ -43,60 +51,63 @@ import java_cup.runtime.*;
 	"use"		{ return symbol(Sym.USE);	 }
 	"length"	{ return symbol(Sym.LENGTH); }
 	"true"		{ return symbol(Sym.TRUE);	 }
-	"false"		{ return symbol(Sym.FALSE);	 } 
+	"false"		{ return symbol(Sym.FALSE);	 }
 
 	/* Symbols */
-    "-"			{ return symbol(Sym.MINUS);      } 
-    "!"			{ return symbol(Sym.BANG);       } 
+    "-"			{ return symbol(Sym.MINUS);      }
+    "!"			{ return symbol(Sym.BANG);       }
     "*"			{ return symbol(Sym.STAR);       }
-    "*>>"		{ return symbol(Sym.HIGHMULT);   }           
-    "/"			{ return symbol(Sym.DIV);        }           
-    "%"			{ return symbol(Sym.MOD);        }           
-    "+"			{ return symbol(Sym.PLUS);       }           
-    "="			{ return symbol(Sym.EQ);         }           
-    "<"			{ return symbol(Sym.LT);         }           
-    "<="		{ return symbol(Sym.LTE);        }           
-    ">="		{ return symbol(Sym.GTE);        }           
-    ">"			{ return symbol(Sym.GT);         }           
-    "=="		{ return symbol(Sym.EQEQ);       }           
-    "!="		{ return symbol(Sym.NEQ);        }           
-    "&"			{ return symbol(Sym.AMP);        }           
-    "|"			{ return symbol(Sym.BAR);        }           
-    ";"			{ return symbol(Sym.SEMICOLON);  }           
-    "("			{ return symbol(Sym.LPAREN);     }           
-    ")"			{ return symbol(Sym.RPAREN);     }           
-    "["			{ return symbol(Sym.LBRACKET);   }           
-    "]"			{ return symbol(Sym.RBRACKET);   }           
-    "{"			{ return symbol(Sym.LBRACE);     }           
-    "}"			{ return symbol(Sym.RBRACE);     }           
-    "_"			{ return symbol(Sym.UNDERSCORE); }   
+    "*>>"		{ return symbol(Sym.HIGHMULT);   }
+    "/"			{ return symbol(Sym.DIV);        }
+    "%"			{ return symbol(Sym.MOD);        }
+    "+"			{ return symbol(Sym.PLUS);       }
+    "="			{ return symbol(Sym.EQ);         }
+    "<"			{ return symbol(Sym.LT);         }
+    "<="		{ return symbol(Sym.LTE);        }
+    ">="		{ return symbol(Sym.GTE);        }
+    ">"			{ return symbol(Sym.GT);         }
+    "=="		{ return symbol(Sym.EQEQ);       }
+    "!="		{ return symbol(Sym.NEQ);        }
+    "&"			{ return symbol(Sym.AMP);        }
+    "|"			{ return symbol(Sym.BAR);        }
+    ";"			{ return symbol(Sym.SEMICOLON);  }
+    "("			{ return symbol(Sym.LPAREN);     }
+    ")"			{ return symbol(Sym.RPAREN);     }
+    "["			{ return symbol(Sym.LBRACKET);   }
+    "]"			{ return symbol(Sym.RBRACKET);   }
+    "{"			{ return symbol(Sym.LBRACE);     }
+    "}"			{ return symbol(Sym.RBRACE);     }
+    "_"			{ return symbol(Sym.UNDERSCORE); }
     ","			{ return symbol(Sym.COMMA);      }
-    ":"			{ return symbol(Sym.COLON);      }           
-                
+    ":"			{ return symbol(Sym.COLON);      }
+
 	/* String */
-	\"			{ sb.setLength(0); yybegin(STRING); }         
+	\"			{ sb.setLength(0);
+                  startColumn = column();
+                  yybegin(STRING); }
 
 	/* Character */
 	\'			{ sb.setLength(0); yybegin(CHARACTER); }
 
 	/* whitespace */
-}               
-               
+}
+
 <STRING> {
 	/* End of string */
 	\"			 { yybegin(YYINITIAL);
-				   return symbol(Sym.STRING,
-				   sb.toString());}
-	[^\n\r\"\\]+ { sb.append( yytext() ); }
+                   int c = startColumn;
+                   startColumn = -1;
+				   return new Symbol(Sym.STRING, row(), c, sb.toString()); }
+	[^\n\r\"\\]+ { sb.append(yytext()); }
 
-	/* escape characters */	
+	/* escape characters */
 	\\t			 { sb.append('\t');		  }
 	\\n			 { sb.append('\n');		  }
 	\\r			 { sb.append('\r');		  }
 	\\\"		 { sb.append('\"');		  }
 	\\			 { sb.append('\\');		  }
 
-} 
+}
 
 <CHARACTER> {
 	/* end of character */
@@ -105,5 +116,5 @@ import java_cup.runtime.*;
 				  sb.toString());}
 }
 
-                
+
 [^] { throw new Error("Illegal character <"+ yytext()+">"); }
--- test/mjw297/LexerTest.java
+++ test/mjw297/LexerTest.java
@@ -1,36 +1,22 @@
 package mjw297;
 
-import java.io.StringReader;
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 import java_cup.runtime.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class LexerTest {
-
 	private Symbol eof = new Symbol(Sym.EOF, -1, -1);
 
-	private void assertSymEquals(Symbol expected, Symbol actual) {
-		assertSymEquals(Arrays.asList(expected), Arrays.asList(actual));
-	}
-
-	// TODO: check rows and cols
-	
-    private void assertSymEquals(List<Symbol> expecteds, List<Symbol> actuals) {
-        assertEquals("Error: number of tokens not equal.", expecteds.size(), actuals.size());
-        for (int i = 0; i < expecteds.size(); ++i) {
-            Symbol expected = expecteds.get(i);
-            Symbol actual = actuals.get(i);
-            assertEquals("Error: symbol codes not equal.", expected.sym, actual.sym);
-            assertEquals("Error: symbol values not equal.", expected.value, actual.value);
-			assertEquals("Error: symbol left index not equal.", expected.left, actual.left);
-			assertEquals("Error: symbol right index not equal.", expected.right, actual.right);
-        }
-    }
-	
+    /**
+     * {@code lex(s)} uses the {@code Lexer} class to repeatedly lex tokens
+     * from {@code s} until the EOF token is reached. The EOF token <i>is</i>
+     * included in the returned list.
+     */
 	private List<Symbol> lex(String s) throws IOException {
 		List<Symbol> result = new ArrayList<>();
 		Lexer l = new Lexer(new StringReader(s));
@@ -45,6 +31,44 @@ public class LexerTest {
 		result.add(sym);
 		return result;
 	}
+
+    /**
+     * {@code assertSymEquals(e, a)} asserts that the symbol codes, values,
+     * line and column number of {@code e} and {@code a} are equal.
+     */
+	private void assertSymEquals(Symbol expected, Symbol actual) {
+		assertSymEquals(Arrays.asList(expected), Arrays.asList(actual));
+	}
+
+    /**
+     * {@code assertSymEquals(e1..en, a1..am)} asserts that the {@code n == m}
+     * and that for all {@code i} in {@code 1..n}, {@code assertSymEquals(ei,
+     * ai)}.
+     */
+    private void assertSymEquals(List<Symbol> expecteds, List<Symbol> actuals) {
+        assertEquals("Error: number of tokens not equal.", expecteds.size(), actuals.size());
+        for (int i = 0; i < expecteds.size(); ++i) {
+            Symbol expected = expecteds.get(i);
+            Symbol actual = actuals.get(i);
+            assertEquals("Error: symbol codes not equal.", expected.sym, actual.sym);
+            assertEquals("Error: symbol values not equal.", expected.value, actual.value);
+			assertEquals("Error: symbol left index not equal.", expected.left, actual.left);
+			assertEquals("Error: symbol right index not equal.", expected.right, actual.right);
+        }
+    }
+
+    /**
+     * {@code assertLexedStringEquals(e, s)} asserts that {@code lex(s) == [new
+     * Symbol(Sym.STRING, 1, 1, e), eof]}. That is, it asserts that {@code s}
+     * is lexed into exactly two tokens. The second is the EOF token. The first
+     * a string token starting at row 1 and column 1 and with contents {@code
+     * e}. This method makes testing string lexing much simpler.
+     */
+    private void assertLexedStringEquals(String expected, String s) throws IOException {
+        assertSymEquals(
+            Arrays.asList(new Symbol(Sym.STRING, 1, 1, expected), eof),
+            lex(s));
+    }
 
     @Test
     public void symbolEqualsTest() throws IOException {
@@ -79,7 +103,7 @@ public class LexerTest {
 			new Symbol(Sym.IF    , 1, 6),
 			new Symbol(Sym.ELSE  , 1, 8),
 			new Symbol(Sym.RETURN, 1, 12),
-			new Symbol(Sym.INT   , 1, 18), 
+			new Symbol(Sym.INT   , 1, 18),
 			new Symbol(Sym.BOOL  , 1, 21),
 			new Symbol(Sym.USE   , 1, 25),
 			new Symbol(Sym.LENGTH, 1, 28),
@@ -97,31 +121,31 @@ public class LexerTest {
 		//          12345678901234567890123456789012
 		String s = "-!**>>/%+=<<=>=>!===&|;()[]{}_,:";
 		List<Symbol> expecteds = Arrays.asList(
-			new Symbol(Sym.MINUS,     1, 1), 
-			new Symbol(Sym.BANG,      1, 2), 
-         	new Symbol(Sym.STAR,      1, 3), 
-         	new Symbol(Sym.HIGHMULT,  1, 4), 
-         	new Symbol(Sym.DIV,       1, 7), 
-         	new Symbol(Sym.MOD,       1, 8), 
-         	new Symbol(Sym.PLUS,      1, 9), 
-         	new Symbol(Sym.EQ,        1, 10), 
-         	new Symbol(Sym.LT,        1, 11), 
-         	new Symbol(Sym.LTE,       1, 12), 
-         	new Symbol(Sym.GTE,       1, 14), 
-         	new Symbol(Sym.GT,        1, 16), 
-         	new Symbol(Sym.NEQ,       1, 17), 
-         	new Symbol(Sym.EQEQ,      1, 19), 
-         	new Symbol(Sym.AMP,       1, 21), 
-         	new Symbol(Sym.BAR,       1, 22), 
-         	new Symbol(Sym.SEMICOLON, 1, 23), 
-         	new Symbol(Sym.LPAREN,    1, 24), 
-         	new Symbol(Sym.RPAREN,    1, 25), 
-         	new Symbol(Sym.LBRACKET,  1, 26), 
-         	new Symbol(Sym.RBRACKET,  1, 27), 
-         	new Symbol(Sym.LBRACE,    1, 28), 
-         	new Symbol(Sym.RBRACE,    1, 29), 
-         	new Symbol(Sym.UNDERSCORE,1, 30), 
-         	new Symbol(Sym.COMMA,     1, 31), 
+			new Symbol(Sym.MINUS,     1, 1),
+			new Symbol(Sym.BANG,      1, 2),
+         	new Symbol(Sym.STAR,      1, 3),
+         	new Symbol(Sym.HIGHMULT,  1, 4),
+         	new Symbol(Sym.DIV,       1, 7),
+         	new Symbol(Sym.MOD,       1, 8),
+         	new Symbol(Sym.PLUS,      1, 9),
+         	new Symbol(Sym.EQ,        1, 10),
+         	new Symbol(Sym.LT,        1, 11),
+         	new Symbol(Sym.LTE,       1, 12),
+         	new Symbol(Sym.GTE,       1, 14),
+         	new Symbol(Sym.GT,        1, 16),
+         	new Symbol(Sym.NEQ,       1, 17),
+         	new Symbol(Sym.EQEQ,      1, 19),
+         	new Symbol(Sym.AMP,       1, 21),
+         	new Symbol(Sym.BAR,       1, 22),
+         	new Symbol(Sym.SEMICOLON, 1, 23),
+         	new Symbol(Sym.LPAREN,    1, 24),
+         	new Symbol(Sym.RPAREN,    1, 25),
+         	new Symbol(Sym.LBRACKET,  1, 26),
+         	new Symbol(Sym.RBRACKET,  1, 27),
+         	new Symbol(Sym.LBRACE,    1, 28),
+         	new Symbol(Sym.RBRACE,    1, 29),
+         	new Symbol(Sym.UNDERSCORE,1, 30),
+         	new Symbol(Sym.COMMA,     1, 31),
          	new Symbol(Sym.COLON,     1, 32),
 			eof
 		);
@@ -136,16 +160,113 @@ public class LexerTest {
 			new Symbol(Sym.WHILE, 1, 2),
 			eof
 		);
-		
+
 		assertSymEquals(expecteds, lex("+while"));
 	}
 
 	@Test
-	public void stringTest() throws IOException {
-		Lexer  l = new Lexer(new StringReader("\"hello\t\""));
-		Symbol s = l.next_token();
-		Symbol expected = new Symbol(Sym.STRING, 1, 8, "hello\t");
-        assertSymEquals(expected, s);
-		
+	public void singleStringTest() throws IOException {
+        /* simple strings with spaces */
+        assertLexedStringEquals("", "\"\"");
+        assertLexedStringEquals("a", "\"a\"");
+        assertLexedStringEquals("ab", "\"ab\"");
+        assertLexedStringEquals("abc", "\"abc\"");
+        assertLexedStringEquals("a c", "\"a c\"");
+        assertLexedStringEquals("  this  is  a  string  ",
+                                "\"  this  is  a  string  \"");
+
+        /* nefarious strings without escape characters */
+        // no spaces
+        assertLexedStringEquals("while",   "\"while\"");
+        assertLexedStringEquals("if",      "\"if\"");
+        assertLexedStringEquals("else",    "\"else\"");
+        assertLexedStringEquals("return",  "\"return\"");
+        assertLexedStringEquals("int",     "\"int\"");
+        assertLexedStringEquals("bool",    "\"bool\"");
+        assertLexedStringEquals("use",     "\"use\"");
+        assertLexedStringEquals("length",  "\"length\"");
+        assertLexedStringEquals("true",    "\"true\"");
+        assertLexedStringEquals("false",   "\"false\"");
+        // 1 space on left
+        assertLexedStringEquals(" while",   "\" while\"");
+        assertLexedStringEquals(" if",      "\" if\"");
+        assertLexedStringEquals(" else",    "\" else\"");
+        assertLexedStringEquals(" return",  "\" return\"");
+        assertLexedStringEquals(" int",     "\" int\"");
+        assertLexedStringEquals(" bool",    "\" bool\"");
+        assertLexedStringEquals(" use",     "\" use\"");
+        assertLexedStringEquals(" length",  "\" length\"");
+        assertLexedStringEquals(" true",    "\" true\"");
+        assertLexedStringEquals(" false",   "\" false\"");
+        // 1 space on right
+        assertLexedStringEquals("while ",   "\"while \"");
+        assertLexedStringEquals("if ",      "\"if \"");
+        assertLexedStringEquals("else ",    "\"else \"");
+        assertLexedStringEquals("return ",  "\"return \"");
+        assertLexedStringEquals("int ",     "\"int \"");
+        assertLexedStringEquals("bool ",    "\"bool \"");
+        assertLexedStringEquals("use ",     "\"use \"");
+        assertLexedStringEquals("length ",  "\"length \"");
+        assertLexedStringEquals("true ",    "\"true \"");
+        assertLexedStringEquals("false ",   "\"false \"");
+        // 1 space on left and right
+        assertLexedStringEquals(" while ",   "\" while \"");
+        assertLexedStringEquals(" if ",      "\" if \"");
+        assertLexedStringEquals(" else ",    "\" else \"");
+        assertLexedStringEquals(" return ",  "\" return \"");
+        assertLexedStringEquals(" int ",     "\" int \"");
+        assertLexedStringEquals(" bool ",    "\" bool \"");
+        assertLexedStringEquals(" use ",     "\" use \"");
+        assertLexedStringEquals(" length ",  "\" length \"");
+        assertLexedStringEquals(" true ",    "\" true \"");
+        assertLexedStringEquals(" false ",   "\" false \"");
+        // lots of spaces
+        assertLexedStringEquals("   while    ",   "\"   while    \"");
+        assertLexedStringEquals("   if    ",      "\"   if    \"");
+        assertLexedStringEquals("   else    ",    "\"   else    \"");
+        assertLexedStringEquals("   return    ",  "\"   return    \"");
+        assertLexedStringEquals("   int    ",     "\"   int    \"");
+        assertLexedStringEquals("   bool    ",    "\"   bool    \"");
+        assertLexedStringEquals("   use    ",     "\"   use    \"");
+        assertLexedStringEquals("   length    ",  "\"   length    \"");
+        assertLexedStringEquals("   true    ",    "\"   true    \"");
+        assertLexedStringEquals("   false    ",   "\"   false    \"");
+
+        assertLexedStringEquals("-",    "\"-\"");
+        assertLexedStringEquals("!",    "\"!\"");
+        assertLexedStringEquals("*",    "\"*\"");
+        assertLexedStringEquals("*>>",  "\"*>>\"");
+        assertLexedStringEquals("/",    "\"/\"");
+        assertLexedStringEquals("%",    "\"%\"");
+        assertLexedStringEquals("+",    "\"+\"");
+        assertLexedStringEquals("=",    "\"=\"");
+        assertLexedStringEquals("<",    "\"<\"");
+        assertLexedStringEquals("<=",   "\"<=\"");
+        assertLexedStringEquals(">=",   "\">=\"");
+        assertLexedStringEquals(">",    "\">\"");
+        assertLexedStringEquals("==",   "\"==\"");
+        assertLexedStringEquals("!=",   "\"!=\"");
+        assertLexedStringEquals("&",    "\"&\"");
+        assertLexedStringEquals("|",    "\"|\"");
+        assertLexedStringEquals(";",    "\";\"");
+        assertLexedStringEquals("(",    "\"(\"");
+        assertLexedStringEquals(")",    "\")\"");
+        assertLexedStringEquals("[",    "\"[\"");
+        assertLexedStringEquals("]",    "\"]\"");
+        assertLexedStringEquals("{",    "\"{\"");
+        assertLexedStringEquals("}",    "\"}\"");
+        assertLexedStringEquals("_",    "\"_\"");
+        assertLexedStringEquals(",",    "\",\"");
+        assertLexedStringEquals(":",    "\":\"");
+
+        assertLexedStringEquals(
+              "- !  * *>> / % + = < <= >= > == != & | ; () [ ] { } _ , :",
+            "\"- !  * *>> / % + = < <= >= > == != & | ; () [ ] { } _ , :\"");
+        assertLexedStringEquals(
+              "-!**>>/%+=<<=>=>==!=&|;()[]{}_,:",
+            "\"-!**>>/%+=<<=>=>==!=&|;()[]{}_,:\"");
+        assertLexedStringEquals(
+              "use io main(args: int[][]) { print(12); c3p0: int = 12; }",
+            "\"use io main(args: int[][]) { print(12); c3p0: int = 12; }\"");
 	}
 }

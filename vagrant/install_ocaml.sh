226b1ca7f56824525c881914b72908a0dbedfbe1
Michael Whittaker <mjw297@cornell.edu>
2016-02-24 23:11:08 -0500

    Updated ocaml installation script.

--- vagrant/install_ocaml.sh
+++ vagrant/install_ocaml.sh
@@ -1,5 +1,6 @@
 sudo apt-get install aspcud m4 unzip
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - \
-    | sh -s /usr/local/bin
+    | sh -s /usr/local/bin 4.02.3
 eval `opam config env`
+opam init
 opam install core async

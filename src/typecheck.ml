d1ea54be9e3a3f25ebdad0c6e4dc128c6f98f9ac
Ralph Recto <recto.ralph@gmail.com>
2016-02-27 18:21:35 -0500

    expr_typecheck is now compiling. Unclear about constructor shadowing warning.

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -17,81 +17,93 @@ type sigma = Var of t
 
 type context = sigma String.Map.t
 
+let dummy () = ()
+
 let string_of_binopcode op =
-  op |> Ast.sexp_of_binopcode |> Sexp.to_string
+  op |> Ast.sexp_of_binop_code |> Sexp.to_string
 
 let string_of_unopcode op =
-  op |> Ast.sexp_of_unopcode |> Sexp.to_string
+  op |> Ast.sexp_of_unop_code |> Sexp.to_string
 
-let rec expr_typecheck (c: context) ((p, expr): Pos.expr)  : ((t Ast.expr) * (Pos * string)) Result.t =
+let rec expr_typecheck (c : context) ((p, expr) : Pos.expr) =
   match expr with
-  | Int n -> OK (IntT, expr)
-  | Bool b -> OK (BoolT, expr)
-  | String s -> OK (ArrayT IntT, expr)
-  | Char c -> OK (IntT, expr)
-  | Array [] -> OK (EmptyArray, expr)
+  | Int _ -> Ok (IntT, expr)
+  | Bool _ -> Ok (BoolT, expr)
+  | String _ -> Ok (ArrayT IntT, expr)
+  | Char _ -> Ok (IntT, expr)
+  | Array [] -> Ok (EmptyArray, expr)
   | Array (hd::tl) -> begin
-    expr_typecheck c hd >>= fun (typ, e) ->
-      let tl_res = List.map ~f:(expr_typecheck c) tl in
-      if List.for_all (fun (y_typ, _) -> typ = y_typ) tl_res then
-        OK (ArrayT typ, expr)
-      else
-        Error (p, "Array elements have different type.")
+    expr_typecheck c hd >>= fun (typ, _) ->
+    List.map ~f:(expr_typecheck c) tl |> Result.all >>= fun typlist ->
+      if List.for_all ~f:(fun (el_typ, _) -> el_typ = typ) typlist then
+        Ok (ArrayT typ, expr)
+      else Error (p, "Array elements have different types")
   end
   | Id s -> begin
     match String.Map.find c s with
     (* just variables - no function lookup *)
-    | Some (Var typ) -> OK (typ, expr)
+    | Some (Var typ) -> Ok (typ, expr)
     | _ -> Error (p, Printf.sprintf "Variable %s unbound" s)
   end
-  | BinOp (e1,(MINUS|STAR|HIGHMULT|DIV|MOD) as opcode,e2) -> begin
-    expr_typecheck c e1 >>= fun (typ1, e1') ->
-    expr_typecheck c e2 >>= fun (typ2, e2') ->
-      match typ1, typ2 with
-      | IntT, IntT -> OK (IntT, expr)
+  | BinOp (e1, opcode, e2) -> begin
+    expr_typecheck c e1 >>= fun (typ1, _) ->
+    expr_typecheck c e2 >>= fun (typ2, _) ->
+      match typ1, typ2, opcode with
+      | IntT, IntT, (MINUS|STAR|HIGHMULT|DIV|MOD) -> Ok (IntT, expr)
+      | IntT, IntT, (LT|LTE|GTE|GT|EQEQ|NEQ) -> Ok (BoolT, expr)
+      | BoolT, BoolT, (AMP|BAR|EQEQ|NEQ) -> Ok (BoolT, expr)
+      | (ArrayT _ | EmptyArray), (ArrayT _ | EmptyArray), (EQEQ|NEQ) -> Ok (BoolT, expr)
+      | IntT, IntT, PLUS -> Ok (IntT, expr)
+      | ArrayT t1, ArrayT t2, PLUS when t1 = t2 -> Ok (ArrayT t1, expr)
+      | ArrayT t, EmptyArray, PLUS
+      | EmptyArray, ArrayT t, PLUS -> Ok (ArrayT t, expr)
+      | EmptyArray, EmptyArray, PLUS -> Ok (EmptyArray, expr)
       | _ ->
-        let ops = string_of_binopcode opcode in
-        Error (p, Printf.sprintf "Expecting int operands for %s" ops)
-  end
-  | BinOp (e1, PLUS, e2) -> begin
-    expr_typecheck c e1 >>= fun (typ1, e1') ->
-    expr_typecheck c e2 >>= fun (typ2, e2') ->
-      match typ1, typ2 with
-      | IntT, IntT -> OK (IntT, expr)
-      | ArrayT t1, Array t2 when t1 = t2 -> OK (ArrayT t1, expr)
-      | ArrayT t, EmptyArray
-      | EmptyArray, ArrayT t -> OK (ArrayT t, expr)
-      | EmptyArray, EmptyArray -> OK (EmptyArray, expr)
-      | _ -> Error (p, "Inconsistent operand types for +")
+          let binop_str = string_of_binopcode opcode in
+          Error (p, Printf.sprintf "Wrong operand types for %s" binop_str)
   end
   | UnOp (opcode, e) -> begin
-    expr_typecheck c e >>= fun (typ, e') ->  
+    expr_typecheck c e >>= fun (typ, _) ->  
       match opcode, typ with
-      | UMINUS, IntT -> OK (IntT, expr)
-      | BANG, BoolT -> OK (BoolT, expr)
+      | UMINUS, IntT -> Ok (IntT, expr)
+      | BANG, BoolT -> Ok (BoolT, expr)
       | _ ->
-        let emsg = Printf.sprintf "Inconsistent operand type for %s" opcode in
-        Error (p, emsg)
+          let unop_str = string_of_unopcode opcode in
+          Error (p, Printf.sprintf "Wrong operand type for %s" unop_str)
   end
   | Index (e1, e2) -> begin
-    expr_typecheck c e1 >>= fun (typ1, e1') ->
-    expr_typecheck c e2 >>= fun (typ2, e2') ->
+    expr_typecheck c e1 >>= fun (typ1, _) ->
+    expr_typecheck c e2 >>= fun (typ2, _) ->
       match typ1, typ2 with
-      | ArrayT t, IntT -> OK (t, expr)
-      | EmptyArray, _ -> Error (p, "Indexing into empty array")
-      | _ -> Error (p, "Bad indexing into array")
+      | ArrayT t, IntT -> Ok (t, expr)
+      | EmptyArray, IntT -> Error (p, "Indexing into empty array")
+      | _, IntT -> Error (p, "Indexing into non-array value")
+      | (ArrayT _ | EmptyArray), _ -> Error (p, "Non-integer index")
+      | _ -> Error (p, "Invalid types for indexing expr")
   end
   | Length e -> begin
-    expr_typecheck c e >>= fun (typ, (_, e')) ->
+    expr_typecheck c e >>= fun (typ, _) ->
       match typ with
-      | Array _ | EmptyArray -> OK (IntT, expr)
-      | _ -> Error (p, "Using length() on a non-array")
+      | ArrayT _ | EmptyArray -> Ok (IntT, expr)
+      | _ -> Error (p, "Using length() on a non-array expr")
   end
   | FuncCall ((_, name), args) -> begin
     match String.Map.find c name, args with
-    | Function (UnitT, t), [] when t <> Unit -> OK (t, expr)
-    | Function (TupleT lst, t), _ :: _ :: tl when t <> Unit ->
-        List.zip lst args 
-    | Function (t1, t2) ->
+    | Some (Function (UnitT, t)), [] when t <> UnitT -> Ok (t, expr)
+    | Some (Function (TupleT lst, t)), _ :: _ :: _ when t <> UnitT -> begin
+      List.map ~f:(expr_typecheck c) args |> Result.all >>= fun typlist ->
+        List.zip lst typlist |> function
+          | Some zipped ->
+              if List.for_all ~f:(fun (t1, (t2, _)) -> t1 = t2) zipped then
+                Ok (t, expr)
+              else Error (p, "Function args do not match parameter type")
+          | None -> Error (p, "Incorrect number of arguments")
+    end
+    | Some (Function (t1, t2)), arg :: [] when t2 <> UnitT -> begin
+      expr_typecheck c arg >>= function
+        | arg_t, _ when arg_t = t1 -> Ok (t2, expr)
+        | _ -> Error (p, "Function arg does not match parameter type")
+    end
+    | None, _ -> Error (p, Printf.sprintf "Variable %s not in scope" name)
+    | _ -> Error (p, "Function call type error")
   end
-

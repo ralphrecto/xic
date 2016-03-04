2b465820f543157d3d9c498ab0d5f147aa4dc6e5
seungseung <sh969@cornell.edu>
2016-03-04 01:42:43 +0000

    starting callable test

--- test/typecheckTest.ml
+++ test/typecheckTest.ml
@@ -22,13 +22,20 @@ let empty_c = Context.empty
 let (|-) c e = (c, e)
 
 (* If <: is subtype, then =: is equal type. *)
-let (=:) ((c, e): context * Pos.expr) (t: Expr.t) : unit =
+let (e=:) ((c, e): context * Pos.expr) (t: Expr.t) : unit =
   let b = is_ok (expr_typecheck c e >>| fun e' -> assert_equal (fst e') t) in
   assert_true b
 
 let test_expr () =
-    empty_c |- (p, Int 42L) =: IntT;
-    ()
+  empty_c |- (p, Int 42L) e=: IntT;
+  ()
+
+
+let (f=:) ((c, e): context * Pos.callable) (func_t: Expr.t * Expr.t) : unit =
+	let b = is_ok (fst_func_pass c e >>= fun gamma ->
+								 snd_func_pass gamma e >>= fun (t,_) -> Ok (assert_equal t func_t)) in
+	assert_true b
+	
 
 (* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! *)
 (* ! ADD YOUR TESTS ADD YOUR TESTS ADD YOUR TESTS ADD YOUR TESTS ADD YOUR TEST *)

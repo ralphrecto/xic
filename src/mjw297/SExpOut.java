80eab87290efe3abb5a98bf74cb878e69a4923aa
Ralph Recto <recto.ralph@gmail.com>
2016-03-04 17:33:43 -0500

    making interface parsing work

--- src/mjw297/SExpOut.java
+++ src/mjw297/SExpOut.java
@@ -357,7 +357,6 @@ class SExpOut implements Ast.NodeVisitor<Position, Void> {
 
     public Void visit(Ast.Interface<Position> i) {
         printer.startList();
-        i.uses.forEach(u -> u.accept(this));
         i.fs.forEach(f -> f.accept(this));
         printer.endList();
         return null;

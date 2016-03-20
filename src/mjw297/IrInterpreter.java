e959791cefb6391c9b6e0f64d7f9ffea6b4b9002
Michael Whittaker <mjw297@cornell.edu>
2016-03-20 00:44:15 +0000

    Added ir interp

---
+++ src/mjw297/IrInterpreter.java
@@ -0,0 +1,23 @@
+package mjw297;
+
+import java.io.FileReader;
+
+import edu.cornell.cs.cs4120.xic.ir.IRCompUnit;
+import edu.cornell.cs.cs4120.xic.ir.interpret.IRSimulator;
+import edu.cornell.cs.cs4120.xic.ir.parse.IRLexer;
+import edu.cornell.cs.cs4120.xic.ir.parse.IRParser;
+
+public class IrInterpreter {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1)  {
+            System.err.println("usage: xi ir_file");
+            System.exit(-1);
+        }
+
+        String filename = args[0];
+        IRLexer lexer = new IRLexer(new FileReader(filename));
+        IRParser parser = new IRParser(lexer);
+        IRSimulator sim = new IRSimulator((IRCompUnit) parser.parse().value);
+        sim.call("_Imain_paai", 0l);
+    }
+}

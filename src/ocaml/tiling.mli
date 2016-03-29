c64f4922bbeb9be66871140cbaecf5df345d1e9f
Michael Whittaker <mjw297@cornell.edu>
2016-03-29 17:33:13 +0000

    Added tiling mli.

---
+++ src/ocaml/tiling.mli
@@ -0,0 +1,7 @@
+open Core.Std
+
+module FreshReg   : Fresh.S
+module FreshLabel : Fresh.S
+
+val munch_expr : Ir.expr -> Asm.abstract_reg * Asm.abstract_asm list
+val munch_stmt : Ir.stmt -> Asm.abstract_asm list

ef83db5b6647e40d572e8d2593b3758fcc1629d3
Michael Whittaker <mjw297@cornell.edu>
2016-03-29 19:24:45 +0000

    WIP abstract to concrete

--- src/ocaml/tiling.mli
+++ src/ocaml/tiling.mli
@@ -5,3 +5,5 @@ module FreshLabel : Fresh.S
 
 val munch_expr : Ir.expr -> Asm.abstract_reg * Asm.abstract_asm list
 val munch_stmt : Ir.stmt -> Asm.abstract_asm list
+
+val register_allocate : Asm.abstract_asm list -> Asm.asm list

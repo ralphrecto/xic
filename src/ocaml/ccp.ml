d23351907e914b8795d8bf2a077e60e2801a2be3
sh969@cornell.edu <sh969@cornell.edu>
2016-04-25 20:19:40 -0400

    ccp now compiles hurrayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- src/ocaml/ccp.ml
+++ src/ocaml/ccp.ml
@@ -20,21 +20,28 @@ module CCPLattice = struct
       | Def i1, Def i2 when Int64.equal i1 i2 -> def1
       | _ -> Overdef
     in
-    let f temp def1 acc =
+    let f ~key:temp ~data:def1 acc =
       let def2 = String.Map.find_exn defined2 temp in
       let new_def = defined_meet def1 def2 in
-      String.Map.add acc ~key:temp new_def
+      String.Map.add acc ~key:temp ~data:new_def
     in
-    let new_defined = String.Map.fold ~f ~init:String.Map.empty defined2 in
+    let new_defined = String.Map.fold ~f ~init:String.Map.empty defined1 in
     match reach1, reach2 with
     | Unreach, Unreach -> (Unreach, new_defined)
     | _ -> (Reach, new_defined)
 
   let ( === ) (reach1, defined1) (reach2, defined2) =
+    let def_equal def1 def2 =
+      match def1, def2 with
+      | Undef, Undef -> true
+      | Def i1, Def i2 -> i1 = i2
+      | Overdef, Overdef -> true
+      | _ -> false
+    in
     match reach1, reach2 with
-    | Undef, Undef -> String.Map.equal defined1 defined2
-    | Def i1, Def i2 -> i1 = i2 && String.Map.equal defined1 defined2
-    | Overdef, Overdef -> String.Map.equal defined1 defined2
+    | Reach, Reach
+    | Unreach, Unreach -> String.Map.equal def_equal defined1 defined2
+    | _ -> false
 
   let to_string (reach, defined) =
     let def_to_string = function
@@ -42,7 +49,7 @@ module CCPLattice = struct
       | Def i -> "def " ^ (Int64.to_string i)
       | Overdef -> "overdef"
     in
-    let f temp def acc =
+    let f ~key:temp ~data:def acc =
       acc ^ (Printf.sprintf "%s: %s," temp (def_to_string def))
     in
     let defined_string = String.Map.fold ~f ~init:"" defined in

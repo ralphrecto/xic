632b9a5071bf83ce1e987fd122616be44fa4bc76
sh969@cornell.edu <sh969@cornell.edu>
2016-04-27 01:29:21 -0400

    adding ccp mli file

---
+++ src/ocaml/ccp.mli
@@ -0,0 +1,23 @@
+module Long = Int64
+open Core.Std
+open Cfg
+open Dataflow
+open Ir
+open Tiling
+open Fresh
+
+module CcpLattice : sig
+  type reachable = Reach | Unreach
+  type defined = Undef | Def of Int64.t | Overdef
+  include Dataflow.LowerSemilattice with
+  type data = (reachable * defined String.Map.t)
+end
+
+module CcpCFG : sig
+  module Lattice = CcpLattice
+  module CFG = IrCfg
+end
+
+val ccp: Ir.stmt list -> Ir.stmt list
+val ccp_comp_unit : Ir.comp_unit -> Ir.comp_unit
+
